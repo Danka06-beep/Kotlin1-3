d7023bcf602e5f3b0113e7ed3d8baec773c28cf1
Danka02 <kamikoto02@mail.ru>
2021-10-11 17:45:34 +0300

    j

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,17 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val post = Post(1,"Dan","10 october","la la la", like = true, comment = false, share = false )
-
-
+        val post = Post(1,"Dan",453878,"la la la", like = true, comment = false, share = true,3,0,1 )
+        inthat(post)
     }
-    fun isTrue(post : Post){
+    private fun isTrue(post : Post) {
         if (post.like) {
             likeImgBtn.setImageResource(R.drawable.ic_baseline_favorite_24)
         }
@@ -22,6 +21,51 @@ class MainActivity : AppCompatActivity() {
         if (post.share) {
             shareImgBtn.setImageResource(R.drawable.ic_baseline_share_24)
         }
+
     }
 
+    fun inthat(post: Post) {
+
+        authorxt.text = post.author
+        txt.text = post.txt
+        if (post.likeTxt > 0) {
+            likeTxt.text = post.likeTxt.toString()
+        }
+        if (post.commentTxt > 0) {
+            commentTxt.text = post.commentTxt.toString()
+        }
+        if (post.shareTxt > 0) {
+            shareTxt.text = post.shareTxt.toString()
+        }
+
+        datetxt.text = date(post)
+        isTrue(post)
+
+    }
+
+    private fun date(post: Post): String {
+        val publishedAgo =  post.data / 1000
+        val toMin = if (publishedAgo > 3599) {
+            publishedAgo / 3600
+        } else {
+            publishedAgo / 60
+        }
+        return when (publishedAgo) {
+            in 0..59 -> "менее минуты назад"
+            in 60..179 -> "минуту назад"
+            in 180..299 -> "$toMin минуты назад"
+            in 300..3599 -> "$toMin минут назад"
+            in 3600..17999 -> "$toMin часа назад"
+            else -> "$toMin часов назад "
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/myapplication/Post.kt
+++ app/src/main/java/com/example/myapplication/Post.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication
 
-class Post(val id: Int, val author: String, val data: String, val txt: String,val like: Boolean, val comment: Boolean, val share: Boolean ) {
+class Post(val id: Int, val author: String, val data: Long, val txt: String,val like: Boolean, val comment: Boolean, val share: Boolean, val likeTxt : Int, val commentTxt : Int, val shareTxt : Int ) {
 
 
 }
